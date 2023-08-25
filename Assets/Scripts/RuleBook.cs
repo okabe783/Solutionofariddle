4e5357c09f96efef7c23e0785d5c9b8a048f74ae
okabe783 <134473737+okabe783@users.noreply.github.com>
2023-08-25 23:11:55 +0900

    剣と盾のスキルを作成した

--- Assets/Scripts/RuleBook.cs
+++ Assets/Scripts/RuleBook.cs
@@ -8,8 +8,26 @@ public class RuleBook : MonoBehaviour
         CardType myCard = CardType.None;
         EnemyType enemyType = EnemyType.None;
 
-        // カードの評価を行うここから
-        if (myCard.HasFlag(CardType.Sword) && enemyType.HasFlag(EnemyType.Green))
+        // カードの評価を行う
+        //プレイヤーのカードが剣で敵の魔石が赤、青、緑の時
+        if (myCard.HasFlag(CardType.Sword) && (enemyType.HasFlag(EnemyType.Red) || (enemyType.HasFlag(EnemyType.Blue))
+            || (enemyType.HasFlag(EnemyType.Green))))
+        {
+            _enemy.Life -= 1;
+        }
+        //プレイヤーのカードが剣で敵の魔石が黄色の時
+        if (myCard.HasFlag(CardType.Sword) && (enemyType.HasFlag(EnemyType.Yellow)))
+        {
+            _player.Life -= 1;
+        }
+        //プレイヤーのカードが盾で敵の魔石が赤、青、黄色の時
+        if (myCard.HasFlag(CardType.Shield) && (enemyType.HasFlag(EnemyType.Red) || (enemyType.HasFlag(EnemyType.Blue))
+            || (enemyType.HasFlag(EnemyType.Yellow))))
+        {
+            _enemy.Life -= 1;
+        }
+        //プレイヤーの武器が盾で敵の魔石が緑の時
+        if (myCard.HasFlag(CardType.Shield) && (enemyType.HasFlag(EnemyType.Green)))
         {
             _player.Life -= 1;
         }
