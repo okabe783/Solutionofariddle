d05a09f75eb43edc2d1e28034705330fe2a041df
okabe783 <134473737+okabe783@users.noreply.github.com>
2023-08-29 23:40:24 +0900

    Unity上でカードが生成されるようにした

--- Assets/Scripts/Card/CardGenerator.cs
+++ Assets/Scripts/Card/CardGenerator.cs
@@ -11,17 +11,18 @@ public class CardGenerator : MonoBehaviour
     public Card Spawn(SpawnType spawnType)
     {
         int randomIndex = 0;
+        Card card = Instantiate(cardprefab);
         if (spawnType == SpawnType.Enemy)
         {
              randomIndex = Random.Range(0, _enemycardBases.Length);
+            card.Set(_enemycardBases[randomIndex]);
         }
         else
         {
              randomIndex = Random.Range(0, _playercardBases.Length);
-
+            card.Set(_playercardBases[randomIndex]);
         }
-        Card card = Instantiate(cardprefab);
-        card.Set(_playercardBases[randomIndex]);
+        
         return card;
     }
     public enum SpawnType
