a80329faef302d588f3ec5bf3b60978e799556b9
okabe783 <134473737+okabe783@users.noreply.github.com>
2023-09-21 22:58:45 +0900

    カードが拡大されるようにした

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+    public float hoverScale = 1.2f; // マウスを合わせたときの拡大率
+    private Vector3 originalScale; // オブジェクトの元のスケール
+
+    private void Start()
+    {
+        originalScale = transform.localScale; // オブジェクトの元のスケールを保存
+    }
+
+    private void OnMouseEnter()
+    {
+        // マウスがオブジェクトに入ったときの処理
+        transform.localScale = originalScale * hoverScale; // 拡大する
+    }
+
+    private void OnMouseExit()
+    {
+        // マウスがオブジェクトから出たときの処理
+        transform.localScale = originalScale; // 元のスケールに戻す
+    }
+}
