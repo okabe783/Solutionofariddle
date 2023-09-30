bf1e28842e0e1a92ff1ea1b883534c8f525eca61
okabe783 <134473737+okabe783@users.noreply.github.com>
2023-09-30 23:05:20 +0900

    ビルド用準備

---
+++ Assets/Scripts/TitleManager/PauseManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+   public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
